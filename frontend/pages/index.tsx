6dcd3437a6d788d3c6781c3666e8245e3abf3f60
Abheek Tripathy <abheek.tripathy@gmail.com>
2022-12-14 03:38:55 +0530

    frontend-setup

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { HeaderAction } from '../components/header'
+
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+
+  return (
+    
+     <HeaderAction links={[]}>
+      
+     </HeaderAction>
+   
+  )
+}
